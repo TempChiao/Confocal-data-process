2a77dcd1221b1c99eb3602855874f24c8d92ff2d
Tianxiao Zhao <s2279999@ed.ac.uk>
2024-03-11 15:06:35 +0000

    FRET program can now be used to threshold the data, report number of events and plot a random time span of plot.

--- FRET_confocal.py
+++ FRET_confocal.py
@@ -7,14 +7,18 @@ Created on Wed Feb 21 12:26:33 2024
 
 
 import numpy as np
-
+import random
+import matplotlib.pyplot as plt
 
 #where to work
-path = 'D:/20240222_CE/1_1_2_294030/'
+path = 'D:/20240223_CE/10_10_2930/'
 root = '488'
 # how many files were collected
 file_numbers=10
-
+frequency = 1000
+aquisition_time = 30
+# please put in how long in seconds do you want to visualise the data
+display_range = 5 
 # Threshold, change here to set different threshold
 green_threshold = 10
 red_threshold = 10
@@ -37,6 +41,20 @@ def read_data(path):
     data = np.genfromtxt(path, delimiter = '\t')
     return data
 
+def generate_random_range():
+    global file_numbers
+    global aquisition_time
+    global frequency
+    global display_range
+    total_events = file_numbers *  aquisition_time *  frequency
+    display = display_range* frequency
+    random_integer = random.randint(0,total_events-display)
+    end = random_integer + display
+    return(random_integer,end)
+    
+    
+    
+    
 red_data = []
 green_data = []
 for i in file_list:
@@ -82,3 +100,10 @@ else:
     print('The number of green event is {}'.format(green_count))
     print('The number of FRET event is {}'.format(Fret_count))
 
+start,end = generate_random_range()
+
+negative_green_list = [-x for x in green_data[start:end]]
+plt.figure(figsize=(18, 6))
+plt.xlabel('*100us')
+plt.plot(range(start,end),negative_green_list, color = 'green')
+plt.plot(range(start,end),red_data[start:end], color = 'red')
